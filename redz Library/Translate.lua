d05d8684fa0d89117f430528cc001871a5d464a0
agenext <lucasviniciussoaresdasilva100@gmail.com>
2025-09-02 22:07:27 -0300

    Add files via upload

---
+++ redz Library/Translate.lua
@@ -0,0 +1,59 @@
+Translations = {
+    ["BR"] = {
+	    ["994732206"] = {
+			-- Others --
+			["Social Media"] = "Redes Sociais",
+			["Farm"] = "Farmar",
+			["Quests/Items"] = "Missões/Itens",
+			["Sea Events"] = "Eventos Marítimos",
+			["Islands"] = "Ilhas",
+			["Fruits/Raid"] = "Frutas/Raid",
+			["Teleport"] = "Teletransporte",
+			["Stats"] = "Estatísticas",
+			["Player"] = "Jogador",
+			["Race"] = "Raça",
+			["Shop"] = "Loja",
+			["Visual"] = "Visual",
+			["Misc"] = "Misc",
+			-- Social --
+			["Someone Hub | Community"] = "Someone Hub | Comunidade",
+			["Join The Best Roblox Scripting Community"] = "Junte-Se A Melhor Comunidade De Scripts Do Roblox",
+			["Someone Hub Official Channel"] = "Someone Hub Canal Oficial",
+			["Sign Up To Receive Updates About The Script As Soon As Possible."] = "Se Inscreva Para Receber Novidades Sobre O Script O Mais Rápido Possível",
+			-- Misc --
+			["Settings"] = "Configurações",
+			["Codes"] = "Códigos",
+			["Menus"] = "Menus",
+			["Team"] = "Time",
+			["More FPS"] = "Mais FPS",
+			["Others"] = "Outros",
+			-- Farm --
+			["Chest"] = "Baú",
+			["Ectoplasm"] = "Ectoplasma",
+			["Bones"] = "Ossos",
+			["Eagle Boss"] = "Chefe Água",
+			["Elite Hunters"] = "Caçadores De Elite",
+			["Bosses"] = "Chefes",
+			["Mastery"] = "Maestria",
+			-- Quests --
+			-- Sea Events --
+			-- Islands --
+			-- Fruits --
+			-- Teleport --
+			-- Stats --
+			-- Player --
+			-- Race --
+			-- Shop --
+			-- Visual --
+			
+		},
+	},
+    ["TH"] = {
+		[""] = {
+		},
+	},
+    ["VN"] = {
+		[""] = {
+		},
+	},
+}
