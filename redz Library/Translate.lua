05412111e0f10eee4d9b729610a8a4da85f93d46
agenext <lucasviniciussoaresdasilva100@gmail.com>
2025-09-04 20:31:01 -0300

    Add files via upload

---
+++ redz Library/Translate.lua
@@ -0,0 +1,104 @@
+Translations = {
+    ["BR"] = {
+	    ["994732206"] = {
+			-- Others --
+			["Social Media"] = "Redes Sociais",
+			["Farm"] = "Farmar",
+			["Quests/Items"] = "Missões/Itens",
+			["Sea Events"] = "Eventos Marítimos",
+			["Islands"] = "Ilhas",
+			["Fruits/Raid"] = "Frutas/Raid",
+			["Teleport"] = "Teletransporte",
+			["Stats"] = "Estatísticas",
+			["Player"] = "Jogador",
+			["Race"] = "Raça",
+			["Shop"] = "Loja",
+			["Visual"] = "Visual",
+			["Misc"] = "Miscelânea",
+			-- Social --
+			["Someone Hub | Community"] = "Someone Hub | Comunidade",
+			["Join The Best Roblox Scripting Community"] = "Junte-Se A Melhor Comunidade De Scripts Do Roblox",
+			["Someone Hub Official Channel"] = "Someone Hub Canal Oficial",
+			["Sign Up To Receive Updates About The Script As Soon As Possible."] = "Se Inscreva Para Receber Novidades Sobre O Script O Mais Rápido Possível",
+			-- Misc --
+			["Settings"] = "Configurações",
+			["Codes"] = "Códigos",
+			["Menus"] = "Menus",
+			["Team"] = "Time",
+			["More FPS"] = "Mais FPS",
+			["Others"] = "Outros",
+			-- Farm --
+			["Chest"] = "Baú",
+			["Ectoplasm"] = "Ectoplasma",
+			["Bones"] = "Ossos",
+			["Eagle Boss"] = "Chefe Água",
+			["Elite Hunters"] = "Caçadores De Elite",
+			["Bosses"] = "Chefes",
+			["Mastery"] = "Maestria",
+			-- Quests --
+			["Second Sea"] = "Segundo Mar",
+			["Third Sea"] = "Terceiro Mar",
+			["Don Swan"] = "Don Swan",
+			["Bartilo Quest"] = "Missão Do Bartilo",
+			["Citizen Quest"] = "Missão Do Cidadão",
+			["Law Boss"] = "Chefe Law",
+			["Barista Cousin"] = "Primo Do Barista",
+			["Berries"] = "Bagas",
+			-- Sea Events --
+			["Settings"] = "Configurações",
+			-- Islands --
+			["Kitsune Island"] = "Ilha Da Kitsune",
+			["Mirage Island"] = "Ilha Da Miragem",
+			["Prehistoric Island"] = "Ilha Pre-Historica",
+			["Frozen Dimension"] = "Dimensão Congelada",
+			-- Fruits --
+			["Fruits"] = "Frutas",
+			-- Teleport --
+			["Seas"] = "Mares",
+			-- Stats --
+			-- Player --
+			["Local Player"] = "Seu Personagem",
+			["Aimbot Nearest"] = "Aimbot No Mais Próximo",
+			["Target The Nearest Player"] = "Mira No Jogador Mais Próximo",
+			-- Race --
+			["To Evolve"] = "Evoluir",
+			["Unlock Races"] = "Desbloquear Race",
+			-- Shop --
+			["Christmas"] = "Natal",
+			["Valentine's Day"] = "Dia Dos Namorados",
+			["Ectoplasm"] = "Ectoplasma",
+			["Misc"] = "Miscelânea",
+			["Fighting Styles"] = "Estilos De Luta",
+			["Swords"] = "Espadas",
+			["Guns"] = "Armas De Fogo",
+			["Accessories"] = "Acessórios",
+			["Races"] = "Raças",
+			["Volcano Event"] = "Evento Do Vulcão",
+			["Beast Hunter"] = "Caçador De Feras",
+			["Shark Hunter"] = "Caçador De Tubarões",
+			["Dragon Talon Sage"] = "Sábio Garra De Dragão",
+			-- Visual --
+			["Extra Sensory Perception"] = "Percepção Extra Sensorial",
+			["Font Size"] = "Tamanho Da Fonte",
+			["Font Size To Be Used"] = "Tamanho Da Fonte Que Será Usada",
+			["Select Font"] = "Selecione A Fonte",
+			["Choose The Font That Will Be Used In The ESPs"] = "Escolha A Fonte Que Será Usada Nos ESPs",
+			["ESP Players"] = "ESP Jogadores",
+			["See Name, Distance, Health And Energy"] = "Veja Nome, Distância, Saúde E Energia",
+			["ESP Fruits"] = "ESP Frutas",
+			["See Name And Distance"] = "Veja Nome E Distância",
+			["ESP Berries"] = "ESP Bagas",
+			["ESP Chests"] = "ESP Baús",
+			["ESP Islands"] = "ESP Ilhas",
+			["ESP Flowers"] = "ESP Flores",
+		},
+	},
+    ["TH"] = {
+		[""] = {
+		},
+	},
+    ["VN"] = {
+		[""] = {
+		},
+	},
+}
